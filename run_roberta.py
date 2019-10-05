456fa12310208bcd2303c6fee2470861461d771e
rujunhan <rujunhan@qsubmit.isi.edu>
2019-10-05 12:55:00 -0700

    add

---
+++ run_roberta.py
@@ -0,0 +1,197 @@
+# coding=utf-8                                                                                                           
+# Copyright 2018 The Google AI Language Team Authors and The HugginFace Inc. team.                                       
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.                                                          
+#                                                                                                                        
+# Licensed under the Apache License, Version 2.0 (the "License");                                                        
+# you may not use this file except in compliance with the License.                                                       
+# You may obtain a copy of the License at                                                                                
+#                                                                                                                        
+#     http://www.apache.org/licenses/LICENSE-2.0                                                                         
+#                                                                                                                        
+# Unless required by applicable law or agreed to in writing, software                                                    
+# distributed under the License is distributed on an "AS IS" BASIS,                                                      
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                               
+# See the License for the specific language governing permissions and                                                    
+# limitations under the License.                                                                                         
+"""BERT finetuning runner."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import csv
+import os
+import logging
+import argparse
+import random
+from tqdm import tqdm, trange
+
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+
+from tokenization import RoBertaTokenizer
+from modeling import BertForSequenceClassification
+from optimization import BertAdam
+from file_utils import PYTORCH_PRETRAINED_BERT_CACHE
+from modeling import BertMultiwayMatch
+from run_multiway_att import SwagExample, DataProcessor, CommonsenseQaProcessor
+
+def main():
+        parser = argparse.ArgumentParser()
+
+    ## Required parameters                                                                                         
+    parser.add_argument("--data_dir",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
+    parser.add_argument("--roberta_model", default=None, type=str, required=True,
+                        help="Roberta pre-trained model selected in the list: roberta-base, "
+                             "roberta-large, roberta-large-mnli. ")
+    parser.add_argument("--task_name",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="The name of the task to train.")
+    parser.add_argument("--output_dir",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="The output directory where the model predictions and checkpoints will be written.")
+
+    ## Other parameters                                                                                            
+    parser.add_argument("--max_seq_length",
+                        default=128,
+                        type=int,
+                        help="The maximum total input sequence length after WordPiece tokenization. \n"
+                             "Sequences longer than this will be truncated, and sequences shorter \n"
+                             "than this will be padded.")
+    parser.add_argument("--do_train",
+                        action='store_true',
+                        help="Whether to run training.")
+    parser.add_argument("--do_eval",
+                        action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_lower_case",
+                        action='store_true',
+                        help="Set this flag if you are using an uncased model.")
+    parser.add_argument("--train_batch_size",
+                        default=32,
+                        type=int,
+                        help="Total batch size for training.")
+    parser.add_argument("--eval_batch_size",
+                        default=8,
+                        type=int,
+                        help="Total batch size for eval.")
+    parser.add_argument("--learning_rate",
+                        default=5e-5,
+                        type=float,
+                        help="The initial learning rate for Adam.")
+    parser.add_argument("--num_train_epochs",
+                        default=3.0,
+                        type=float,
+                        help="Total number of training epochs to perform.")
+    parser.add_argument("--warmup_proportion",
+                        default=0.1,
+                        type=float,
+                        help="Proportion of training to perform linear learning rate warmup for. "
+                             "E.g., 0.1 = 10%% of training.")
+    parser.add_argument("--no_cuda",
+                        action='store_true',
+                        help="Whether not to use CUDA when available")
+    parser.add_argument("--local_rank",
+                        type=int,
+                        default=-1,
+                        help="local_rank for distributed training on gpus")
+    parser.add_argument('--seed',
+                        type=int,
+                        default=42,
+                        help="random seed for initialization")
+    parser.add_argument('--gradient_accumulation_steps',
+                        type=int,
+                        default=1,
+                        help="Number of updates steps to accumulate before performing a backward/update pass.")
+    parser.add_argument('--fp16',
+                        action='store_true',
+                        help="Whether to use 16-bit float precision instead of 32-bit")
+    parser.add_argument('--loss_scale',
+                        type=float, default=0,
+                        help="Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n"
+                             "0 (default value): dynamic loss scaling.\n"
+                             "Positive power of 2: static loss scaling value.\n")
+    parser.add_argument('--cuda',
+                        type=str,
+                        default="",
+                        help="cuda index")
+
+    args = parser.parse_args()
+
+    processors = {
+        "commonsenseqa": CommonsenseQaProcessor,
+    }
+
+    num_labels_task = {
+        "commonsenseqa":4,
+    }
+
+    if args.local_rank == -1 or args.no_cuda:
+        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+        n_gpu = torch.cuda.device_count()
+    else:
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        n_gpu = 1
+        # Initializes the distributed backend which will take care of sychronizing nodes/GPUs                      
+        torch.distributed.init_process_group(backend='nccl')
+
+    logger.info("device: {} n_gpu: {}, distributed training: {}, 16-bits training: {}".format(
+        device, n_gpu, bool(args.local_rank != -1), args.fp16))
+    print("device: {} n_gpu: {}, distributed training: {}, 16-bits training: {}".format(
+        device, n_gpu, bool(args.local_rank != -1), args.fp16))
+
+    if args.gradient_accumulation_steps < 1:
+        raise ValueError("Invalid gradient_accumulation_steps parameter: {}, should be >= 1".format(
+            args.gradient_accumulation_steps))
+
+    args.train_batch_size = int(args.train_batch_size / args.gradient_accumulation_steps)
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+    if not args.do_train and not args.do_eval:
+        raise ValueError("At least one of `do_train` or `do_eval` must be True.")
+
+    if os.path.exists(args.output_dir) and os.listdir(args.output_dir) and args.do_train:
+        raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    task_name = args.task_name.lower()
+
+    if task_name not in processors:
+        raise ValueError("Task not found: %s" % (task_name))
+
+    print("current task is " + str(task_name))
+
+
+    processor = processors[task_name]()
+    num_labels = num_labels_task[task_name]
+    label_list = processor.get_labels()
+
+    tokenizer = RoBertaTokenizer.from_pretrained(args.roberta_model, do_lower_case=args.do_lower_case)
+
+    train_examples = None
+    num_train_steps = None
+    if args.do_train:
+        train_examples = processor.get_train_examples(args.data_dir)
+        num_train_steps = int(
+            len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps * args.num_train_epochs)
+
+    # to pick up here...
+
+if __name__ == "__main__":
+    main()
