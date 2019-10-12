962f8be5f66a4723d3d41d25c7be3243c96a08fc
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-12 05:24:25 +0000

    sent_len = 180, dev acc = 79.1%

--- grid_search_roberta.sh
+++ grid_search_roberta.sh
@@ -3,18 +3,18 @@ source activate
 
 task="commonsenseqa"
 
-batchsizes=( 24 )
+batchsizes=( 60 )
 for s in "${batchsizes[@]}"
 do
-    learningrates=( 2e-5 )
+    learningrates=( 1e-5 )
 
     for l in "${learningrates[@]}"
     do
-        epochs=( 1 )
+        epochs=( 2 )
 
         for e in "${epochs[@]}"
         do
-            python run_roberta.py --task_name "${task}" --do_eval --do_train --do_lower_case --roberta_model roberta-large --data_dir data/ --max_seq_length 220 --train_batch_size ${s} --learning_rate ${l} --num_train_epochs ${e}  --gradient_accumulation_steps=4  --output_dir output/batch_${s}_lr_${l}_epochs${e}_seed_42 --seed 42 --fp16
+            python run_roberta.py --task_name "${task}" --do_eval --do_train --do_lower_case --roberta_model roberta-large --data_dir data/ --max_seq_length 180 --train_batch_size ${s} --learning_rate ${l} --num_train_epochs ${e}  --gradient_accumulation_steps=10  --output_dir output/batch_${s}_lr_${l}_epochs${e}_seed_42 --seed 42 --fp16
         done
     done
 done
